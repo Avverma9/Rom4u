91ac574b73dd8b7bcb058e15ab1651758f2251a7
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-06 13:56:14 +0530

    update

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -20,7 +20,7 @@ function NavScrollExample() {
 
   useEffect(() => {
     console.log("WebSocket connecting...");
-    const ws = new WebSocket("ws://https://youtube-bbrv.onrender.com"); // Update the URL if needed
+    const ws = new WebSocket("ws://https://youtube-bbrv.onrender.com");
   
     ws.onopen = () => {
       console.log("WebSocket connected!");
