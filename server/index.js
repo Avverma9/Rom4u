e2d23c5a2788f963505c7b5dedfab335b29e8c86
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-06 12:50:17 +0530

    done

--- server/index.js
+++ server/index.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-
-const route = require("./routes/route.js")
-
+const route = require("./routes/route.js");
 
 const app = express();
 app.use(cors());
@@ -19,22 +17,32 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI =
   "mongodb+srv://Avverma:Avverma95766@avverma.2g4orpk.mongodb.net/Youtube";
 
-
-
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+// WebSocket logic
+let activeUsers = 0;
 
-  app.use("/", route);
-  // app.use("/", emailController)
-  
+io.on("connection", (socket) => {
+  activeUsers++;
 
-  app.use((req, res) => {
-      return res.status(400).send({ status: false, message: "End point is incorrect" })
+  // Send the updated active user count to all connected clients
+  io.emit("activeUsers", activeUsers);
+
+  socket.on("disconnect", () => {
+    activeUsers--;
+
+    // Send the updated active user count to all connected clients
+    io.emit("activeUsers", activeUsers);
   });
+});
 
+app.use("/", route);
 
-  
+app.use((req, res) => {
+  return res.status(400).send({ status: false, message: "End point is incorrect" });
+});
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
