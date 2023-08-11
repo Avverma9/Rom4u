80b942e1f0c0a0755aba876b6dce09a31758160a
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-11 11:10:21 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./component/Footer/Footer";
+import NavScrollExample from "./component/Header/Header";
+import LandingPage from "./component/LandingPage/Landingpage";
+import VideoPage from "./component/Youtube/Video";
+import VideoMI11T from "./component/Youtube/mi11t";
+import VideoMI11x from "./component/Youtube/mi11x";
+import Tool from "./component/Youtube/tools";
+import Sidebar from "./component/Admin/Sidebar";
+import Requests from "./component/Youtube/Requests";
+import Rom from "./component/Youtube/rom";
+
+import Profile from "./component/Profile/Profile.jsx"; // Add the import statement for Profile
+import Login from "./component/Authentication/Loginpage";
+import Signup from "./component/Authentication/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavScrollExample />
+
+      <Routes>
+        <Route path="/requests" element={<Requests />} />
+        <Route path="/videos" element={<VideoPage />} />
+        <Route path="/mi11Tpro" element={<VideoMI11T />} />
+        <Route path="/mi11x" element={<VideoMI11x />} />
+        <Route path="/tools" element={<Tool />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/roms" element={<Rom />} />
+        <Route path="/av" element={<Sidebar />} />
+        <Route path="/register" element={<Signup />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
